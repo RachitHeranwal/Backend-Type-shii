a9c3b8d2abc9d49162a8c80003c2282bd50d195e
RachitHeranwal <frrachit@gmail.com>
2025-11-28 12:14:48 +0530

    Building Server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const fs = require("fs");
 const path = require("path");
 const { log } = require('console');
-
 const port = 3000
 
 const server = http.createServer((req,res) => {
