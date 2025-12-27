0780ac991dc4bc9eb8b6af06b806deb61df21cb0
RachitHeranwal <frrachit@gmail.com>
2025-12-27 18:57:03 +0530

    learned about express

---
+++ 02_express/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+let teadata = [];
+let nextId = 1;
+
+app.post("/teas", (req, res) => {
+  const { name, price } = req.body;
+  const newTea = {
+    id: nextId++,
+    name,
+    price,
+  };
+  teadata.push(newTea);
+  res.status(201).send(newTea);
+});
+
+app.get("/teas", (req, res) => {
+  res.status(200).send(teadata);
+});
+
+app.get("/teas/:id", (req, res) => {
+  const tea = teadata.find((t) => t.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+  res.status(200).send(tea);
+});
+
+// update Tea
+
+app.put("/teas/:id", (req, res) => {
+  const tea = teadata.find((tea) => tea.id === parseInt(req.params.id));
+  if (!tea) {
+    return res.status(404).send("Tea not found");
+  }
+
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
+});
+
+app.delete("/teas/:id", (req, res) => {
+  const index = teadata.findIndex((tea) => tea.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Tea not found");
+  }
+
+  teadata.splice(index, 1);
+  res.status(204).send("deleted");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at port: ${port}`);
+});
